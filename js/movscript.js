e1c5f37bed9aa0df0203eda331e97f1b95bba53b
Chiemeka <chi_aubrey@live.com>
2014-12-08 23:54:10 +0100

    css adjustments

--- js/movscript.js
+++ js/movscript.js
@@ -1,25 +1,24 @@
 var app = {
 	init: function () {
 		var _this = this;
-		this.resultField = $('#result');
+		this.resultField = $('#result'); //defining and assigning resultField
 
 		$('form').submit(function (e) {
-			e.preventDefault();
-			console.log(this);
-			console.log(_this);
+			e.preventDefault(); //preventing default action
 
 			_this.inputField = $('.search-name');
 			_this.inputFieldValue = _this.inputField.val();
 
 			_this.movieSearchBtn = $('#movie-search-btn');
 
+			// API URL and PARAMETERS //
+			//------------------
 			_this.tmdbAPI = 'http://api.themoviedb.org/3/search/movie?';
 			_this.tmdbOptions = {
 				api_key: '21a6a3f0f59f75c9e2ea92921f84ecf2',
 				query: _this.inputFieldValue
 
 			};
-			console.log(_this.tmdbOptions);
 
 			app.retrieveMovie();
 		})
@@ -30,28 +29,28 @@ var app = {
 
 	},
 	
-	displayMovie: function (data) {
-		console.log(data);
-
+	displayMovie: function (data) { //response embedded in data
 		mData = data.results;
 
-		var movieString = '<ul class="answers">';
-		$.each(mData, function (i, movie) {
-			if (movie.poster_path !== null) {
-				movieString += '<li><img src="http://image.tmdb.org/t/p/w500' + movie.poster_path + '" class="image"> ';
-				movieString += 'Title: ' + movie.original_title + '</li>';
-			}
-			// } else {
-					movieString += '<li><img src="imgs/default.png" class="image">';
-					movieString += 'Title: ' + movie.original_title + '</li>';
-			// 	movieString += '<li><img src="imgs/default.png/" style="visibility: hidden"></li>';
-			// }
+		var movieString = '<p class="searchResults">Search Results: </p>';
+		movieString += '<ul class="answers">';
+		$.each(mData, function (i, movie) { //loop through each item in array
+			if (movie.poster_path !== null) { //if item has a picture
 
+				//create a list tag, appending picture and the title
+				movieString += '<li class="movie_title"><img src="http://image.tmdb.org/t/p/w500' + movie.poster_path + '" class="image"> ';
+				movieString += '<span class="image_text">' + movie.original_title + '</span></li>';
+			} else {
+
+				//create a list tag, appending a default image and the title
+				movieString += '<li><img src="imgs/default.png" class="image">';
+				movieString += movie.original_title + '</li>';
+			}
 			
 		});
 		movieString += '</ul>';
 
-		$('#result').html(movieString);
+		$('#result').html(movieString); //insert into result div
 	}
 }
 
